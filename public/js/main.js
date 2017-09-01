188020bed744e2458a51fef9849f5d68e09b7560
Issac Grunwald <yitzygrunwald@gmail.com>
2017-08-31 21:54:06 -0400

    created conditional for results page. Changed search prams to search by title only

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+//create onclick function for button
+//create ajax call to route
+//create route for this call
+//pass the parent/child node as the data
+//insert into db
+//turn off onclick
+//potentially allow for user to unsave/delete
